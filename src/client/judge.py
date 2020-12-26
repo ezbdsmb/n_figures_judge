29dd81e3d0d72b735d8252d0760a130b8ed55a83
khodozzz <khodozzz@gmail.com>
2020-12-26 13:28:59 +0300

    update

--- src/client/judge.py
+++ src/client/judge.py
@@ -27,27 +27,27 @@ class Judge(UDPClient):
     def run(self):
         # send init
         self.sendto('init_judge', self.server_addr)
-        print('send init_judge')
+        # print('send init_judge')
 
         # receive init ok
         data, addr = self.recvfrom()
-        print('received:', data)
+        # print('received:', data)
         # TODO: check ok
 
         # receive board size
         data, addr = self.recvfrom()
         self.board_size = parse_board_size(data)
-        print('received:', data)
+        # print('received:', data)
 
         # receive agents
         data, addr = self.recvfrom()
         self.agents_addr = parse_agents(data)
-        print('received:', data)
+        # print('received:', data)
 
         # send judge
         for agent in self.agents_addr:
             self.sendto(f'judge {str(self.board_size)}', self.agents_addr[agent])
-        print('send judge')
+        # print('send judge')
 
         # send init positions
         mes = 'change_pos '
@@ -55,7 +55,7 @@ class Judge(UDPClient):
             x, y = self.find_new_position(agent)
             mes += f'({agent} {x} {y})'
         self.sendto(mes, self.server_addr)
-        print('send:', mes)
+        # print('send:', mes)
 
         iteration = 0
         while True:
@@ -65,7 +65,7 @@ class Judge(UDPClient):
             collisions = {}
             for i in range(len(self.agents_addr)):
                 data, addr = self.recvfrom()
-                print('received:', data)
+                # print('received:', data)
 
                 coll_key = list(self.agents_addr.keys())[list(self.agents_addr.values()).index(addr)]
                 coll_val = len(data.split(' ')) - 1
@@ -81,9 +81,11 @@ class Judge(UDPClient):
             agent = max_idx[random.randint(0, len(max_idx) - 1)]
             x, y = self.find_new_position(agent)
             self.sendto(f'change_pos ({agent} {x} {y})', self.server_addr)
-            print('send:', f'change_pos ({agent} {x} {y})')
+            # print('send:', f'change_pos ({agent} {x} {y})')
 
-        print(iteration)
+        # send finish
+        self.sendto(f'finish {iteration}', self.server_addr)
+        # print(iteration)
 
 
 if __name__ == '__main__':
